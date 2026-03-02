a34a8a79746d7b1bb4dba81e749356adb6e01cd4
nachikul <43932058+nachikul@users.noreply.github.com>
2026-03-02 15:31:26 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yourdomain.dev',
+  site: 'https://nachikul.dev',
   output: 'static',
   integrations: [sitemap()],
 });
